41182eb03999eec3578cb7bcdb08a3892afea594
Michael <michaelfaleiro@gmail.com>
2024-01-05 23:27:39 -0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -24,6 +24,10 @@ const router = createBrowserRouter([
         path: "/orcamento/:id",
         element: <OrcamentoDetalhes />,
       },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
     ],
   },
 ]);
