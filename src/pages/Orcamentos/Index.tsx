b87ea08d7717d001b0228ba177dcfdf91badb87d
Michael <michaelfaleiro@gmail.com>
2024-01-06 00:11:26 -0300

    deploy

--- src/pages/Orcamentos/Index.tsx
+++ src/pages/Orcamentos/Index.tsx
@@ -62,7 +62,7 @@ export default function Orcamentos() {
                     <td>{item.chassis}</td>
                     <td>{dateFormatter.format(new Date(item.createdAt))}</td>
                     <td>
-                      <Link to={`/orcamento/${item.id}`}>
+                      <Link to={`app-react-painel/orcamento/${item.id}`}>
                         <i className="bi bi-card-list"></i>
                       </Link>
                     </td>
